6f9c6ede08c456f117fd14845c45c8640cefa5c0
fegusta <fegtnc@gmail.com>
2022-03-03 23:18:52 -0300

    layout lista de jogos

--- app/src/main/java/com/fegusta/recyclerview/datasource/DataSource.kt
+++ app/src/main/java/com/fegusta/recyclerview/datasource/DataSource.kt
@@ -11,7 +11,16 @@ class DataSource {
             var jogos = ArrayList<Jogo>()
 
             jogos.add(Jogo(100,"Residente Evil",5,"PS4"))
-            jogos.add(Jogo(200,"Zombie Army",4,"PC"))
+            jogos.add(Jogo(110,"Zombie Army 4",5,"PC"))
+            jogos.add(Jogo(120,"The Last of Us",5,"PS4"))
+            jogos.add(Jogo(130,"Red Dead Redemption",5,"PC"))
+            jogos.add(Jogo(140,"Clash Royal",4,"Mobile"))
+            jogos.add(Jogo(150,"Project Zomboid",5,"PC"))
+            jogos.add(Jogo(160,"Hallo",3,"XBOX"))
+            jogos.add(Jogo(170,"QUantum Break",4,"PC"))
+            jogos.add(Jogo(180,"Civilization",3,"PC"))
+            jogos.add(Jogo(190,"Gears of War 5",2,"XOB-ONE"))
+            jogos.add(Jogo(200,"Slipter.io",5,"PC"))
 
             return jogos
 
